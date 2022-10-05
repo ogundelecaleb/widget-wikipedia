3bde7676af8e8f9dd215f44432370d922341596c
Ogundele Caleb <ogundelecaleb13@gmail.com>
2022-10-05 14:50:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-function App() {
+const items = [
+  {
+    title: 'what is React',
+    content: 'React is frontend Javascript framework'
+  },
+  {
+    title: 'why use React',
+    content: 'React is a favourite JS library among engineers'
+  },
+  {
+    title: 'How do you use React',
+    content: 'You use React by creating component'
+  }
+
+]
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <br />
+      <Search/>
     </div>
   );
-}
-
-export default App;
+};
